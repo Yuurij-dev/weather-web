83a6f918df22f5d69558bc5550ee45701fbd375b
Yuurij-dev <yurijesus.dev@gmail.com>
2025-06-18 01:02:46 -0300

    feat; mudança de quantidade de nuvens

--- src/components/Clouds/Clouds.jsx
+++ src/components/Clouds/Clouds.jsx
@@ -5,7 +5,7 @@ import CloudBackground from '../../assets/cloud-background.png'
 function Clouds() {
   return (
     <div className=''>
-        {[...Array(12)].map((_, i) => (
+        {[...Array(6)].map((_, i) => (
             <div key={i} className={`cloud cloud${i + 1}`}>
                 <img src={CloudBackground} alt="" />
             </div>
